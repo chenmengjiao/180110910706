9a5a87428516e5e9640361b96f4572e9339f4462
chenmengjiao <44493677+chenmengjiao@users.noreply.github.com>
2018-12-21 23:04:42 +0800

    Add files via upload

---
+++ 1221/js/自适应WEB主页媒体查询.js
@@ -0,0 +1,15 @@
+/*
+ * Created by KING on 2017/6/2.
+ */
+function on_html5_click(){
+	document.getElementById("id-image-large").setAttribute("src", "images/html5-256x256.png");
+	document.getElementById("id-image-small").setAttribute("src", "images/html5-64x64.png");
+}
+function on_css3_click(){
+	document.getElementById("id-image-large").setAttribute("src", "images/html5-256x256.png");
+	document.getElementById("id-image-small").setAttribute("src", "images/html5-64x64.png");
+}
+function on_js_click(){
+	document.getElementById("id-image-large").setAttribute("src", "images/html5-256x256.png");
+	document.getElementById("id-image-small").setAttribute("src", "images/html5-64x64.png");
+}
